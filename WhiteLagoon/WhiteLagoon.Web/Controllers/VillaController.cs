7e24e4e3ee4cba8336c535f68b0a2024624087e3
David Riggins <driggins@esri.com>
2025-02-26 10:15:12 -0800

    End Lesson 55

--- WhiteLagoon/WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/VillaController.cs
@@ -46,5 +46,17 @@ namespace WhiteLagoon.Web.Controllers
             return View();
 
         }
+
+
+        public IActionResult Update(int villaId)
+        {
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == villaId);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return View(villa);
+        }
     }
 }
