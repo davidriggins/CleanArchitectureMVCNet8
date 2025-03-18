50c45dc35e70774d089a5d5dd60baa144dcb2f7e
David Riggins <driggins@esri.com>
2025-03-18 11:15:41 -0700

    End Lesson 164

--- WhiteLagoon/WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/BookingController.cs
@@ -52,25 +52,24 @@ namespace WhiteLagoon.Web.Controllers
         [HttpPost]
         public IActionResult FinalizeBooking(Booking booking)
         {
-            var villa = _unitOfWork.Villa.Get(u=>u.Id == booking.VillaId);
+            var villa = _unitOfWork.Villa.Get(u => u.Id == booking.VillaId);
+            booking.TotalCost = villa.Price * booking.Nights;
 
             booking.Status = SD.StatusPending;
             booking.BookingDate = DateTime.Now;
 
-            booking.TotalCost = villa.Price * booking.Nights;
-
             _unitOfWork.Booking.Add(booking);
             _unitOfWork.Save();
-
 
             var domain = Request.Scheme + "://" + Request.Host.Value + "/";
             var options = new SessionCreateOptions
             {
                 LineItems = new List<SessionLineItemOptions>(),
                 Mode = "payment",
-                SuccessUrl = domain + $"/booking/BookingConfirmation?bookingId={booking.Id}.html",
-                CancelUrl = domain + $"/booking/FinalizeBooking?villaId={booking.VillaId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}.html",
+                SuccessUrl = domain + $"booking/BookingConfirmation?bookingId={booking.Id}",
+                CancelUrl = domain + $"booking/FinalizeBooking?villaId={booking.VillaId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}",
             };
+
 
             options.LineItems.Add(new SessionLineItemOptions
             {
@@ -80,29 +79,46 @@ namespace WhiteLagoon.Web.Controllers
                     Currency = "usd",
                     ProductData = new SessionLineItemPriceDataProductDataOptions
                     {
-                        Name = villa.Name,
+                        Name = villa.Name
+                        //Images = new List<string> { domain + villa.ImageUrl },
                     },
                 },
                 Quantity = 1,
             });
 
+
             var service = new SessionService();
             Session session = service.Create(options);
 
-
             _unitOfWork.Booking.UpdateStripePaymentID(booking.Id, session.Id, session.PaymentIntentId);
             _unitOfWork.Save();
-
             Response.Headers.Add("Location", session.Url);
-
             return new StatusCodeResult(303);
 
-            //return RedirectToAction("BookingConfirmation", new { bookingId = booking.Id });
         }
+
 
         [Authorize]
         public IActionResult BookingConfirmation(int bookingId)
         {
+            Booking bookingFromDb = _unitOfWork.Booking.Get(u => u.Id == bookingId,
+                includeProperties: "User,Villa");
+
+            if (bookingFromDb.Status == SD.StatusPending)
+            {
+                //this is a pending order, we need to confirm if payment was successful
+
+                var service = new SessionService();
+                Session session = service.Get(bookingFromDb.StripeSessionId);
+
+                if (session.PaymentStatus == "paid")
+                {
+                    _unitOfWork.Booking.UpdateStatus(bookingFromDb.Id, SD.StatusApproved);
+                    _unitOfWork.Booking.UpdateStripePaymentID(bookingFromDb.Id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.Save();
+                }
+            }
+
             return View(bookingId);
         }
     }
