121aeac011fe70e397e9bbdcff74001b29fa2b6d
David Riggins <driggins@esri.com>
2025-03-17 18:00:58 -0700

    End Lesson 155

--- WhiteLagoon/WhiteLagoon.Application/Common/Utility/SD.cs
+++ WhiteLagoon/WhiteLagoon.Application/Common/Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,12 @@ namespace WhiteLagoon.Application.Common.Utility
     {
         public const string Role_Customer = "Customer";
         public const string Role_Admin = "Admin";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusCheckedIn = "CheckedIn";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
     }
 }
