2839ed41e26a4223a1e2f4cdf64a00a0348450e4
David Riggins <driggins@esri.com>
2025-03-19 10:45:39 -0700

    End Lesson 177

--- WhiteLagoon/WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Web.Models;
 using WhiteLagoon.Web.ViewModels;
 
@@ -45,16 +46,20 @@ namespace WhiteLagoon.Web.Controllers
         //}
 
         [HttpPost]
-        public IActionResult GetVillaSByDate(int nights, DateOnly checkInDate)
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
         {
             var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
 
+            var villaNumbersList = _unitOfWork.VillaNumber.GetAll().ToList();
+            var bookedVillas = _unitOfWork.Booking.GetAll(u => u.Status == SD.StatusApproved ||
+                u.Status == SD.StatusCheckedIn).ToList();
+
             foreach (var villa in villaList)
             {
-                if (villa.Id % 2 == 0)
-                {
-                    villa.IsAvailable = false;
-                }
+                int roomsAvailable = SD.VillaRoomsAvailable_Count(villa.Id, 
+                    villaNumbersList,checkInDate, nights, bookedVillas);
+
+                villa.IsAvailable = roomsAvailable > 0 ? true : false;
             }
 
             HomeVM homeVM = new()
