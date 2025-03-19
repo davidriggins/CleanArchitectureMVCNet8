bddb14febaa45ff5013ae7f239f971ac70522281
David Riggins <driggins@esri.com>
2025-03-19 12:01:31 -0700

    End Lesson 181

--- WhiteLagoon/WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/BookingController.cs
@@ -147,6 +147,20 @@ namespace WhiteLagoon.Web.Controllers
             return View(bookingFromDb);
         }
 
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin)]
+        public IActionResult Checking(Booking booking)
+        {
+            _unitOfWork.Booking.UpdateStatus(booking.Id, SD.StatusCheckedIn, booking.VillaNumber);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Booking Updated Successfully";
+
+            return RedirectToAction(nameof(BookingDetails), new { bookingId = booking.Id });
+        }
+
+
         private List<int> AssignAvailableVillNumberByVilla(int villaId)
         {
             List<int> availableVillaNumbers = new();
