e36e29bcaa31240cba2391a8de784e92c32ca246
David Riggins <driggins@esri.com>
2025-03-20 13:53:03 -0700

    End Lesson 210

--- WhiteLagoon/WhiteLagoon.Web/Controllers/DashboardController.cs
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/DashboardController.cs
@@ -87,6 +87,30 @@ namespace WhiteLagoon.Web.Controllers
         }
 
 
+        public async Task<IActionResult> GetMemberAndBookingLineChartData()
+        {
+            var bookingData = _unitOfWork.Booking.GetAll(u => u.BookingDate >= DateTime.Now.AddDays(-30) &&
+                u.BookingDate <= DateTime.Now)
+                .GroupBy(b=>b.BookingDate.Date)
+                .Select(u => new
+                {
+                    DateTime = u.Key,
+                    NewBookingCount = u.Count(),
+                });
+
+            var customerData = _unitOfWork.User.GetAll(u => u.CreatedAt >= DateTime.Now.AddDays(-30) &&
+                u.CreatedAt <= DateTime.Now)
+                .GroupBy(b => b.CreatedAt.Date)
+                .Select(u => new
+                {
+                    DateTime = u.Key,
+                    NewCustomerCount = u.Count(),
+                });
+
+            return Json(bookingData);
+        }
+
+
         private static RadialBarChartVM GetRadialCartDataModel(int totalCount, double currentMonthCount, double prevMonthCount)
         {
             RadialBarChartVM radialBarChartVM = new();
